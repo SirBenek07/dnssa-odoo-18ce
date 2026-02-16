51fc846f00c36157df791c69cb6a65fae2175beb
Federico Fernandez <fede-fernandez07@hotmail.com>
2026-02-16 09:32:20 -0300

    Añadido modulo contact_extend, que extiende la barra de busqueda para incluir ciudad y departamento

---
+++ conctact_extend/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Conctact Extend",
+    "version": "18.0.1.0.0",
+    "summary": "Extiende la busqueda de contactos por ciudad y departamento",
+    "category": "Contacts",
+    "author": "Federico Fernandez",
+    "license": "LGPL-3",
+    "depends": ["contacts"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
